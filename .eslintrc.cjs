a4c8a8e2e5e1ab51c71d27f0c49114af3610ab62
Vicky De Picard <vickyhotio@gmail.com>
2026-01-11 14:13:22 +0100

    fix(lint): align ESLint config with v8

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:promise/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    "no-unused-vars": ["warn"],
+  },
+};
